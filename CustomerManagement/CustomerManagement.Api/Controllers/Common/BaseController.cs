57683806a97c4777ed76bf3ebaa2f6794b6fcf27
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-07-04 01:23:36 +0000

    Creation and configuration of all API routes and DB script sample added

---
+++ CustomerManagement/CustomerManagement.Api/Controllers/Common/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using CustomerManagement.Api.Models;
+using CustomerManagement.Logic.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerManagement.Api.Controllers.Common
+{
+    public class BaseController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public BaseController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        protected ObjectResult Error(string errorMessage)
+        {
+            return new BadRequestObjectResult(Envelope.Error(errorMessage));
+        }
+        
+        protected new ObjectResult Ok()
+        {
+            _unitOfWork.Commit();
+            return new OkObjectResult(Envelope.Ok());
+        }
+
+        protected ObjectResult Ok<T>(T result)
+        {
+            _unitOfWork.Commit();
+            return new OkObjectResult(Envelope.Ok(result));
+        }
+    }
+}
--- CustomerManagement/CustomerManagement.Api/Startup.cs
+++ CustomerManagement/CustomerManagement.Api/Startup.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CustomerManagement.Logic.Model;
+using CustomerManagement.Logic.Utils;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 
 namespace CustomerManagement.Api
 {
@@ -18,18 +16,46 @@ namespace CustomerManagement.Api
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
 
+            var connectionString = Configuration.GetConnectionString("CustomerManagementDatabase");
+            Initer.Init(connectionString);
+        }
+        
+        public IContainer ApplicationContainer { get; private set; }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            ApplicationContainer = CreateApplicationContainer(services);
+
+            // Creating the IServiceProvider out of the Autofac container
+            return new AutofacServiceProvider(ApplicationContainer);
+        }
+
+        private IContainer CreateApplicationContainer(IServiceCollection services)
+        {
+            // create a Autofac container builder
+            var builder = new ContainerBuilder();
+
+            // read service collection to Autofac
+            builder.Populate(services);
+
+            builder.RegisterType<UnitOfWork>().InstancePerLifetimeScope();
+
+            builder.RegisterType<EmailGateway>()
+                .As<IEmailGateway>()
+                .InstancePerDependency();
+
+            // Build and return the Autofac container
+            return builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -41,7 +67,19 @@ namespace CustomerManagement.Api
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Customer}/{action=Index}");
+            });
+
+            // If you want to dispose of resources that have been resolved in the
+            // application container, register for the "ApplicationStopped" event.
+            // You can only do this if you have a direct reference to the container,
+            // so it won't work with the above ConfigureContainer mechanism.
+            appLifetime.ApplicationStopped.Register(() => ApplicationContainer.Dispose());
         }
     }
 }
