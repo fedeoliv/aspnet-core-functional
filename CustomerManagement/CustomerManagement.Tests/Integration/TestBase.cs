f00b269a7f5eb7f0968123bad7ef9571a54bde42
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-07-09 19:12:37 +0000

    Customer controller integration tests

---
+++ CustomerManagement/CustomerManagement.Tests/Integration/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using CustomerManagement.Logic.Utils;
+using Microsoft.Extensions.Configuration;
+
+namespace CustomerManagement.Tests.Integration
+{
+    public abstract class TestBase
+    {
+        protected TestBase()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json").Build();
+
+            var conn = config.GetConnectionString("CustomerManagementDatabase");
+            
+            ClearDatabase(conn);
+            Initer.Init(conn);
+        }
+
+        private void ClearDatabase(string connectionString)
+        {
+            string query = 
+                "DELETE FROM dbo.Customer;" + 
+                "UPDATE dbo.Ids SET NextHigh = 0";
+
+            using (var cnn = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand(query, cnn)
+                {
+                    CommandType = CommandType.Text
+                };
+
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ CustomerManagement/CustomerManagement.Tests/Utils/CustomerDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CSharpFunctionalExtensions;
+using CustomerManagement.Logic.Model;
+using CustomerManagement.Logic.Utils;
+using Xunit;
+
+namespace CustomerManagement.Tests.Utils
+{
+    public sealed class CustomerDatabase : IDisposable
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public CustomerDatabase()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public Customer ShouldContainCustomer(string name)
+        {
+            var repository = new CustomerRepository(_unitOfWork);
+            Customer customer = repository.GetByName(name);
+
+            Assert.NotNull(customer);
+
+            return customer;
+        }
+
+        public Customer ShouldContainCustomer(long id)
+        {
+            var repository = new CustomerRepository(_unitOfWork);
+            Maybe<Customer> customer = repository.GetById(id);
+
+            Assert.True(customer.HasValue);
+
+            return customer.Value;
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+    }
+}
